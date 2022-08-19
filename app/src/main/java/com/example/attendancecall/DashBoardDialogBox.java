9f1666caa55e89d8d132c92bfd5ad5f03c937dac
her_username <her_email>
2022-08-19 20:43:30 +0530

    commit

--- app/src/main/java/com/example/attendancecall/DashBoardDialogBox.java
+++ app/src/main/java/com/example/attendancecall/DashBoardDialogBox.java
@@ -2,21 +2,31 @@ package com.example.attendancecall;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.ColorDrawable;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.Locale;
 
 public class DashBoardDialogBox {
 
-    public void showDialog(Activity activity, TextView sectionName){
+    public void showDialog(Activity activity, TextView sectionName) {
 
 
         final Dialog dialog = new Dialog(activity);
@@ -49,9 +59,9 @@ public class DashBoardDialogBox {
         teacherSection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (sectionName.getText().toString().trim().toLowerCase(Locale.ROOT).equals("teacher")){
+                if (sectionName.getText().toString().trim().toLowerCase(Locale.ROOT).equals("teacher")) {
                     dialog.dismiss();
-                }else {
+                } else {
                     Intent intent = new Intent(activity, TeacherActivity.class);
                     activity.startActivity(intent);
                     activity.finish();
@@ -63,9 +73,9 @@ public class DashBoardDialogBox {
         studentSection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (sectionName.getText().toString().trim().toLowerCase(Locale.ROOT).equals("student")){
+                if (sectionName.getText().toString().trim().toLowerCase(Locale.ROOT).equals("student")) {
                     dialog.dismiss();
-                }else {
+                } else {
                     Intent intent = new Intent(activity, StudentActivity.class);
                     activity.startActivity(intent);
                     activity.finish();
@@ -104,5 +114,146 @@ public class DashBoardDialogBox {
                 activity.startActivity(intent);
             }
         });
+
+        addNewUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupLogoutForNewUser(activity);
+            }
+        });
+
+        switchAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupLogout(activity);
+            }
+        });
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupLogout(activity);
+            }
+        });
     }
+
+    private void showPopupLogout(Activity activity) {
+
+        final Dialog dialog = new Dialog(activity);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.dialogbox_of_logout);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        Window window = dialog.getWindow();
+        window.setLayout(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT);
+
+        Button btn_ok = (Button) dialog.findViewById(R.id.logout_ok);
+        Button btn_cancel = (Button) dialog.findViewById(R.id.logout_cancel);
+
+        dialog.show();
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EncoderDecoder decoder = new EncoderDecoder();
+
+                FirebaseDatabase db = FirebaseDatabase.getInstance();
+                DatabaseReference root;
+
+                SharedPreferences preferences = dialog.getContext().getSharedPreferences("login_details", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+
+                root = db.getReference().child("admin_users").child(decoder.encodeUserEmail(preferences.getString("email_id","null")));
+                root.child("status").setValue("inactive");
+
+
+                FirebaseAuth fAuth = FirebaseAuth.getInstance();
+//                FirebaseUser firebaseUser = fAuth.getCurrentUser();
+
+
+                SharedPreferences sharedPreferences_isLogin = dialog.getContext().getSharedPreferences("MyPrefsLogin", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor_isLogin = sharedPreferences_isLogin.edit();
+
+                editor_isLogin.putBoolean("hasLoggedIn", false);
+                editor_isLogin.commit();
+
+                editor.clear();
+                editor.apply();
+
+                fAuth.signOut();
+                Intent intent = new Intent(activity, MainActivity.class);
+                activity.startActivity(intent);
+                activity.finish();
+
+            }
+        });
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+    private void showPopupLogoutForNewUser(Activity activity) {
+
+        final Dialog dialog = new Dialog(activity);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.dialogbox_of_logout);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        Window window = dialog.getWindow();
+        window.setLayout(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT);
+
+        Button btn_ok = (Button) dialog.findViewById(R.id.logout_ok);
+        Button btn_cancel = (Button) dialog.findViewById(R.id.logout_cancel);
+
+        dialog.show();
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EncoderDecoder decoder = new EncoderDecoder();
+
+                FirebaseDatabase db = FirebaseDatabase.getInstance();
+                DatabaseReference root;
+
+                SharedPreferences preferences = dialog.getContext().getSharedPreferences("login_details", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+
+                root = db.getReference().child("admin_users").child(decoder.encodeUserEmail(preferences.getString("email_id","null")));
+                root.child("status").setValue("inactive");
+
+                FirebaseAuth fAuth = FirebaseAuth.getInstance();
+//                FirebaseUser firebaseUser = fAuth.getCurrentUser();
+
+                SharedPreferences sharedPreferences_isLogin = dialog.getContext().getSharedPreferences("MyPrefsLogin", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor_isLogin = sharedPreferences_isLogin.edit();
+
+                editor_isLogin.putBoolean("hasLoggedIn", false);
+                editor_isLogin.commit();
+
+                editor.clear();
+                editor.apply();
+
+                fAuth.signOut();
+                Intent intent = new Intent(activity, SignupActivity.class);
+                activity.startActivity(intent);
+                activity.finish();
+            }
+        });
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/attendancecall/MainActivity.java
+++ app/src/main/java/com/example/attendancecall/MainActivity.java
@@ -17,10 +17,20 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean isOldDeviceLoggedIn = false;
+
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference root, checkStatusRoot;
 
     String str_emailId, str_password;
 
@@ -54,8 +64,9 @@ public class MainActivity extends AppCompatActivity {
 
                 invalidDisplay.setText("");
 
-                TextInputLayout emailLayout = (TextInputLayout)findViewById(R.id.login_emailIdLayout);
-                TextInputLayout passwordLayout = (TextInputLayout)findViewById(R.id.login_passwordLayout);
+
+                TextInputLayout emailLayout = (TextInputLayout) findViewById(R.id.login_emailIdLayout);
+                TextInputLayout passwordLayout = (TextInputLayout) findViewById(R.id.login_passwordLayout);
 
                 emailLayout.setErrorEnabled(false);
                 passwordLayout.setErrorEnabled(false);
@@ -63,55 +74,59 @@ public class MainActivity extends AppCompatActivity {
                 str_emailId = emailId.getText().toString().toLowerCase(Locale.ROOT).trim();
                 str_password = password.getText().toString().trim();
 
+                ////////////////////////////////////////////////////////////////////////////////////////
+
+                //////////////////////////////////////////////////////////////////////////////////////////////
+
+
                 boolean allFieldFilled = true;
 
 
-                if(str_emailId.isEmpty()){
+                if (str_emailId.isEmpty()) {
                     emailLayout.setErrorEnabled(true);
                     emailLayout.setError("* Email id field is empty");
                     allFieldFilled = false;
                 }
-                if (str_password.isEmpty()){
+                if (str_password.isEmpty()) {
                     passwordLayout.setErrorEnabled(true);
                     passwordLayout.setError("* Password field is empty");
                     allFieldFilled = false;
                 }
-                if(allFieldFilled){
+                if (allFieldFilled) {
 
                     ValidationOfInput validation = new ValidationOfInput();
                     boolean validUsername = validation.validatedEmailId(str_emailId, emailLayout);
                     boolean validPassword = validation.isValidPasswordForLogin(str_password, passwordLayout);
 
-                    if (validUsername && validPassword){
+                    if (validUsername && validPassword) {
 
-                        mAuth.signInWithEmailAndPassword(str_emailId,str_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        mAuth.signInWithEmailAndPassword(str_emailId, str_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()){
+                                if (task.isSuccessful()) {
                                     emailId.setText("");
                                     password.setText("");
 
                                     verifyingDialogBox dialogBox = new verifyingDialogBox();
-                                    dialogBox.showDialog(MainActivity.this, "Link has been sent to your mail, please click on that link and then click verify email button",str_emailId,str_password);
+                                    dialogBox.showDialog(MainActivity.this, "Link has been sent to your mail, please click on that link and then click verify email button", str_emailId, str_password);
 
                                     // If user click cancel in dialog box
                                     invalidDisplay.setText("* Please try again!");
-                                }else{
+                                } else {
                                     int index = task.getException().toString().indexOf(":");
-                                    String exception = task.getException().toString().toLowerCase(Locale.ROOT).replace(" ","").trim().substring(index + 1);
-                                    if (exception.contains("passwordisinvalid")){
+                                    String exception = task.getException().toString().toLowerCase(Locale.ROOT).replace(" ", "").trim().substring(index + 1);
+                                    if (exception.contains("passwordisinvalid")) {
                                         invalidDisplay.setText("Invalid password!");
-                                    }else if (exception.contains("nouserrecord")){
+                                    } else if (exception.contains("nouserrecord")) {
                                         invalidDisplay.setText("User not exist!");
-                                    }else if (exception.contains("badly")){
+                                    } else if (exception.contains("badly")) {
                                         invalidDisplay.setText("Invalid input!");
-                                    }else{
+                                    } else {
                                         invalidDisplay.setText(task.getException().toString().trim().substring(index + 1));
                                     }
                                 }
                             }
                         });
-
 
                     }
                 }
@@ -123,6 +138,49 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SignupActivity.class);
                 startActivity(intent);
+            }
+        });
+    }
+
+    private void isOldDeviceLoggedInFinder() {
+        final boolean[] isUserExist = {false};
+
+        root = db.getReference().child("admin_users");
+        root.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                EncoderDecoder encoderDecoder = new EncoderDecoder();
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String model = dataSnapshot.getKey();
+
+                    if (model.equals(encoderDecoder.encodeUserEmail(str_emailId))) {
+                        checkStatusRoot = db.getReference().child("admin_users").child(encoderDecoder.encodeUserEmail(str_emailId)).child("status");
+                        checkStatusRoot.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                String strStatus = snapshot.getValue(String.class);
+                                if (strStatus.equals("active")) {
+                                    isOldDeviceLoggedIn = true;
+                                    invalidDisplay.setText("* You already logged in, you need to logout from old device");
+                                    return;
+                                } else {
+                                    return;
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
             }
         });
     }
