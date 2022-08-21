4990029f9f6ccc6d1b060672f5239ffd15011004
her_username <her_email>
2022-08-22 01:08:53 +0530

    sad commit

--- app/src/main/java/com/example/attendancecall/StudentActivity.java
+++ app/src/main/java/com/example/attendancecall/StudentActivity.java
@@ -23,6 +23,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -54,6 +56,16 @@ public class StudentActivity extends AppCompatActivity implements RecyclerViewIn
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
+
+        FirebaseAuth isLoginfAuth = FirebaseAuth.getInstance();
+        FirebaseUser isLoginfUser = isLoginfAuth.getCurrentUser();
+
+        if (isLoginfUser == null){
+            Intent intent = new Intent(StudentActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
 
         sectionName = findViewById(R.id.sectionStudent);
 
--- app/src/main/java/com/example/attendancecall/TeacherActivity.java
+++ app/src/main/java/com/example/attendancecall/TeacherActivity.java
@@ -25,6 +25,7 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -65,6 +66,15 @@ public class TeacherActivity extends AppCompatActivity implements RecyclerViewIn
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
+
+        FirebaseAuth isLoginfAuth = FirebaseAuth.getInstance();
+        FirebaseUser isLoginfUser = isLoginfAuth.getCurrentUser();
+
+        if (isLoginfUser == null){
+            Intent intent = new Intent(TeacherActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
         sectionName = findViewById(R.id.sectionTeacher);
 
